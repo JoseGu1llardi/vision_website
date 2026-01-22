487051d69da71b824e65490759e405715528ab13
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-22 21:23:35 +0000

    fix: adjust padding for PortfolioPage on medium screens

--- app/components/pages/PortfolioPage.tsx
+++ app/components/pages/PortfolioPage.tsx
@@ -7,7 +7,7 @@ import { galleryImages } from "../../data/galleryImages";
 
 export function PortfolioPage() {
   return (
-    <div className="min-h-screen pt-20 md:pt-52">
+    <div className="min-h-screen pt-20 md:pt-40">
       {/* Gallery Carousel */}
       <GalleryCarousel images={galleryImages} />
     </div>
