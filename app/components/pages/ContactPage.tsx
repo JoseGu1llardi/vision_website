82c692795af3905c9beb0b7939cc34ed32f90f37
JoseGu1llardi <junior11_junior@hotmail.com>
2026-01-27 20:12:27 +0000

    fix: update padding in ContactPage component for improved layout on medium screens

--- app/components/pages/ContactPage.tsx
+++ app/components/pages/ContactPage.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export function ContactPage() {
   return (
-    <div className="min-h-screen pt-28 md:pt-40">
+    <div className="min-h-screen pt-28 md:pt-44">
       <FormSection />
     </div>
   );
